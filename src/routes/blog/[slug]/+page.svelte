6e2045f318013f4aad847ae0fc4c65b45827c118
Rishabh Raghuwanshi <Rishabhhsraghuwanshi@gmail.com>
2024-08-24 20:56:04 +0530

    initial commit

--- src/routes/blog/[slug]/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    export let data 
-
-</script>
-
-<p>{data.content}</p>
--- src/routes/blog/[slug]/page.js
+++
@@ -1,8 +0,0 @@
- export function load({params}) {
-    
-    return{
-        content: `hello ${params.slug}`
-
-    }
-    
- }
